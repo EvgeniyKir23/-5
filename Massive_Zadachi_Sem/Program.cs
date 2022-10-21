154856f493c00901d4f740055c21d0b1837cbc1c
Евгений Кириллов <KirillovEE@pssochi.ru>
2022-10-21 15:15:41 +0300

    Save1 Семинары начало работы

---
+++ Massive_Zadachi_Sem/Program.cs
@@ -0,0 +1,13 @@
+﻿
+void PrintNumbers()
+{
+int num1 = 1;
+int num2 = 8;
+// for (int i = num1; i < num2 + 1; i++) Console.Write($"{i}, ");
+for (int i = num1; i <= num2; i++) Console.Write(i+", ");   
+    // if(i % 2 == 0)
+    // {
+    //     Console.Write(i+", ");    
+    // }
+}
+PrintNumbers();
