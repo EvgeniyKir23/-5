1833c58e833e871fd8ee6794fdfb54f3cb0ee174
Евгений Кириллов <KirillovEE@pssochi.ru>
2022-10-28 15:31:23 +0300

    Save2 сохранение практической работы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ВАРИАНТ №3 ПОИСК ИНДЕКСА РУЧНОЙ МЕТОД
+// string[] array = new string[] { "Hello", "2", "world", ":-)" };
+// string[] n = new string[array.Length];
+
+// void SecondArrayWithIF(string[] array, string[] n)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3)
+//         {
+//             n[count] = array[i];
+//             count++;
+//         }
+//     }
+// }
+// void PrintArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Выведение нового МАССИВА из строк, длина которых меньше 3
+
+// string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+// string[] array2 = new string[array1.Length];
+// void SecondArrayWithIF(string[] array1, string[] array2)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array1.Length; i++)
+//     {
+//     if(array1[i].Length <= 3)
+//         {
+//         array2[count] = array1[i];
+//         count++;
+//         }
+//     }
+// }
+// void PrintArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+// SecondArrayWithIF(array1, array2);
+// PrintArray(array2);
---
+++ massive_KR/Program.cs
@@ -0,0 +1,27 @@
+﻿// ПРАКТИЧЕСКОЕ ЗАДАНИЕ ПОИСК ИНДЕКСА РУЧНОЙ МЕТОД
+string[] massive = new string[4]{ "1234", "1567", "-2", "computer science" };
+string[] massive2 = new string[massive.Length];
+
+void SecondArrayWithIF(string[] massive, string[] massive2)
+{
+    int count = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i].Length <= 3)
+        {
+            massive2[count] = massive[i];
+            count++;
+        }
+    }
+}
+
+void Printmassive(string[] massive)
+{
+    for (int i = 0; i < massive.Length; i++)
+    {
+        Console.Write($"{massive[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(massive, massive2);
+Printmassive(massive2);
